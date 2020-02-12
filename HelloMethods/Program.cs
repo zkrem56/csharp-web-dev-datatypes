703962e8a7bb51f074b5920a950761ba8ee8e9ec
Carly Langlois <cj.langlois.cl@gmail.com>
2020-02-12 10:37:26 -0600

    add hellomethods

---
+++ HelloMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloMethods
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = Message.GetMessage("fr");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
