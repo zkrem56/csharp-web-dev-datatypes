76efa5e93699db781de47faedcc3c094a7384072
Carly Langlois <cj.langlois.cl@gmail.com>
2020-02-12 10:33:37 -0600

    add tempconv project

---
+++ TempConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TempConv
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            string input;
+
+            Console.WriteLine("Temperature in F:");
+            input = Console.ReadLine();
+            fahrenheit = double.Parse(input);
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("The Temperature in C is: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp-web-dev-lsn1datatypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_web_dev_lsn1datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
